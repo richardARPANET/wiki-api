ccc51eadbd305cc11cde8fdc91f4a443a94babf9
Richard <38049750+richardARPANET@users.noreply.github.com>
2020-09-15 20:00:25 +0100

    get_tables (#1)
    Co-authored-by: Richard O'Dwyer <richard@richard.do>

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@
 
 from __future__ import absolute_import
 
+from codecs import open
 import os
 
 from setuptools import find_packages, setup
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 
 setup(
@@ -17,24 +21,21 @@ setup(
     packages=find_packages('src', exclude=('tests',)),
     package_dir={'': 'src'},
     include_package_data=True,
-    zip_safe=True,
+    zip_safe=False,
     description=(
-        'A basic python library enabling access to Wikipedia.org\'s '
-        'search results and article content.'),
-    author='Richard O\'Dwyer',
+        "A basic python library enabling access to Wikipedia.org's "
+        'search results and article content.'
+    ),
+    author="Richard O'Dwyer",
     author_email='richard@richard.do',
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description='https://github.com/richardasaurus/wiki-api',
-    install_requires=['requests', 'pyquery', 'beautifulsoup4', 'wheel', 'six'],
+    install_requires=install_requires,
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Internet :: WWW/HTTP'
-    ]
+        'Topic :: Internet :: WWW/HTTP',
+    ],
 )
